dfe9b2dac751be934a02c607fcacda20966f64a9
dexty222 <84411760+dexty222@users.noreply.github.com>
2025-07-27 16:35:00 -0300

    engajadm img

--- src/components/HowItWorks.tsx
+++ src/components/HowItWorks.tsx
@@ -10,7 +10,7 @@ export const HowItWorks = () => {
       color: "text-purple-primary",
       bgColor: "bg-purple-primary/20",
       borderColor: "border-purple-primary/30",
-      imageSrc: "https://i.imgur.com/CMixhKw.png", // imagem corrigida
+      imageSrc: "https://imgur.com/rmcpR9K", // imagem corrigida
       imageAlt: "Imagem do passo 1 - Instalação do template"
     },
     {
