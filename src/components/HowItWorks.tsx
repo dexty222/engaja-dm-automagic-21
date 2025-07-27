db9afcdd160327c6548c92318f03e9bf97b04f02
dexty222 <84411760+dexty222@users.noreply.github.com>
2025-07-27 16:59:10 -0300

    gif

--- src/components/HowItWorks.tsx
+++ src/components/HowItWorks.tsx
@@ -10,7 +10,7 @@ export const HowItWorks = () => {
       color: "text-purple-primary",
       bgColor: "bg-purple-primary/20",
       borderColor: "border-purple-primary/30",
-      imageSrc: "https://imgur.com/a/97UDD0E", // imagem corrigida
+      imageSrc: "https://i.imgur.com/QzzQDLv.gif", // imagem corrigida
       imageAlt: "Imagem do passo 1 - Instalação do template"
     },
     {
